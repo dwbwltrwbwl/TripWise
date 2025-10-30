ebe3bc3cf48d051ae3a6c03bbdddf9b7424a0c7a
dwbwltrwbwl <tyumenelizaveta@yandex.ru>
2025-10-30 19:26:06 +0300

    1926

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TripWise.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
