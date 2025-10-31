711abafeded482caeed0441a575ee49f1f29328d
dwbwltrwbwl <tyumenelizaveta@yandex.ru>
2025-10-31 14:43:23 +0300

    1443

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,19 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TripWise.Models;
 
 namespace TripWise.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly TripWiseContext _context;
+
+        public AccountController(TripWiseContext context)
+        {
+            _context = context;
+        }
+
         // GET: /Account/Login
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string email, string password, bool rememberMe)
+        {
+            // Авторизация временно отключена
+            ModelState.AddModelError("", "Авторизация временно недоступна");
+            return View();
+        }
+
         // GET: /Account/Register
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(string fullName, string email, string password, string confirmPassword, bool agreeTerms)
+        {
+            // Регистрация временно отключена
+            ModelState.AddModelError("", "Регистрация временно недоступна");
             return View();
         }
     }
--- Models/TripWiseContext.cs
+++ Models/TripWiseContext.cs
@@ -45,9 +45,9 @@ public partial class TripWiseContext : DbContext
 
     public virtual DbSet<VotingSystem> VotingSystems { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TripWise;Trusted_Connection=true;TrustServerCertificate=true;");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TripWise;Trusted_Connection=true;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
